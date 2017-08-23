72f50e51743c76c22ee49c720ddebd11fbb8c32e
Andrew Sanchez <bballbizniz@aol.com>
2017-08-22 18:41:20 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// Require Express and Express-Handlebars
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+// Require Mongoose for our DB
+const mongoose = require('mongoose');
+
+// Require Body-Parser to read our body reqs
+const bodyParser = require('body-parser');
